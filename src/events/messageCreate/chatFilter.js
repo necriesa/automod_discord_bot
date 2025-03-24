c7444fcf3e9faa046ebe6994ce916b52babb69b2
NatDy96 <ndcb5050@gmail.com>
2025-03-24 21:01:01 +1100

    FIxed profanity.json path

--- src/events/messageCreate/chatFilter.js
+++ src/events/messageCreate/chatFilter.js
@@ -1,21 +1,23 @@
-const fs = require('fs')
-const badWords = JSON.parse(fs.readFileSync('../../../profanity.json', 'utf8'));
+const fs = require('fs');
+const path = require('path');
 const updateInfraction = require('../../utils/moderation/userInfractions');
 
+const badWords = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../../../profanity.json'), 'utf8')
+);
+
 module.exports = (message) => {
-  // client checks each new message that it can see for bad words
+
   const words = message.content.toLowerCase().split(/\s+/);
 
   if (words.some(word => badWords.includes(word))) {
-    //reply to the message:
     message.channel.send(`${message.author} said a bad word D:`)
       .then(() => console.log(`Filtered a bad word from ${message.author.tag}`))
       .catch(console.error);
-    
-    //delete the message
+
     message.delete().catch(console.error);
 
-    // update infraction info for user
     updateInfraction(message);
   }
-}
+};
+
