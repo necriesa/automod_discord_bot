7b7e2ad03d9766ffe2d188be033ec3b4c0b64a93
necriesa <cslchua12@gmail.com>
2025-03-24 16:41:12 +1100

    fixed console log when bot starts up

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,6 +1,4 @@
 module.exports = (client) => {
   // client logs into console when the bot is ready
-  client.on('ready', (c) =>{
-    console.log(`${c.user.tag} is ready!`);
-  });
+  console.log(`${client.user.tag} is ready!`);
 }
